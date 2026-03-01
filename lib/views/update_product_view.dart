069836f9bdcc5d214b3aa3e4fe510c83d0e2c4b7
RolandoGuZz <guzmancorreonova@gmail.com>
2026-02-28 22:28:46 -0600

    feat: create update view

---
+++ lib/views/update_product_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UpdateProductView extends StatefulWidget {
+  const UpdateProductView({super.key});
+
+  @override
+  State<UpdateProductView> createState() => _UpdateProductViewState();
+}
+
+class _UpdateProductViewState extends State<UpdateProductView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(children: [Text('Update screen')]));
+  }
+}
